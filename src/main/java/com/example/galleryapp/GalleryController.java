c84549ef454cc211d431c7bfda1477f49a398d16
Aysegul-hub <ayseguldelialioglu20@gmail.com>
2026-08-21 17:34:45 +0300

    internDay5

--- src/main/java/com/example/galleryapp/GalleryController.java
+++ src/main/java/com/example/galleryapp/GalleryController.java
@@ -1,4 +1,31 @@
 package com.example.galleryapp;
 
-public class GalleryController {
-}
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+
+    public class GalleryController {
+
+        @FXML
+        private ImageView photo1;
+
+        @FXML
+        private ImageView photo2;
+
+
+        @FXML
+        public void initialize() {
+
+            Image image1 = new Image(getClass().getResource("/com/example/galleryapp/images/photo1.jpeg").toExternalForm());
+            photo1.setImage(image1);
+
+            Image image2 = new Image(getClass().getResource("/com/example/galleryapp/images/photo2.jpg").toExternalForm());
+            photo2.setImage(image2);
+
+
+        }
+
+    }
+
